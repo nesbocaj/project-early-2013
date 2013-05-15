c8778537414b1a8c7a817845e4384bc9ada27e85
Dennis Kong Kei Liu <de.liu@ofir.dk>
2013-05-15 11:15:00 +0200

    Added Web Server, ICityGraph
    Added Web Server with REST connection, and added Interface for CityGraph

---
+++ Web Server/ForwardingService.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Internal_Server;
+
+namespace Web_Server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ForwardingService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ForwardingService.svc or ForwardingService.svc.cs at the Solution Explorer and start debugging.
+    public class ForwardingService : 
+        ClientBase<Internal_Server.ICityGraph>, 
+        Internal_Server.ICityGraph, 
+        IForwardingService
+    {
+        public string[] Cities()
+        {
+            return base.Channel.Cities();
+        }
+
+        public string[] Destination(string initial)
+        {
+            return base.Channel.Destination();
+        }
+
+        public string[] Search(string initial, string destination)
+        {
+            return base.Channel.Search();
+        }
+
+        public string[] Watch(string initial, string destination)
+        {
+            return base.Channel.Watch();
+        }
+    }
+}
---
+++ Web Server/IForwardingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Web_Server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IForwardingService" in both code and config file together.
+    [ServiceContract]
+    public interface IForwardingService
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/list/cities")]
+        string[] Cities();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/list/destinations/{initial}")]
+        string[] Destination(string initial);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/search?From={initial}&To={destination}")]
+        string[] Search(string initial, string destination);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/watch?From={initial}&To={destination}")]
+        string[] Watch(string initial, string destination);
+    }
+}
