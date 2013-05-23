b653663969bfc8d69acafd33c41699a1f6481b12
Nikolaj Lepka <slench102+git@gmail.com>
2013-05-23 13:42:36 +0200

    Added Documentation for the GU

--- Forms Client/View/Main Window.cs
+++ Forms Client/View/Main Window.cs
@@ -14,25 +14,43 @@ namespace Forms_Client.View
     {
         private readonly Presenter.Presenter _presenter = null;
 
+        /// <summary>
+        /// Initializes form, adds the instance of the form to the Presenter, 
+        /// and populates the Comboboxes
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             _presenter = Presenter.Presenter.GetInstance();
-            _presenter.Main = this;
-            _presenter.PopulateLists();
+            _presenter.MainForm = this;
+            _presenter.PopulateFromList();
         }
 
+        /// <summary>
+        /// Calls the View.Overview_Form form from the Presenter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OKButton_Click(object sender, EventArgs e)
         {
             _presenter.CallOverview();
         }
 
-
+        /// <summary>
+        /// Closes the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CancelButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// Takes the content of the comboboxes and returns it in a string array
+        /// if the comboboxes contain a null reference, they're treated as empty
+        /// </summary>
+        /// <returns>An array of strings with the text from the comboboxes</returns>
         public string[] ChosenCities()
         {
             string from, to;
@@ -50,30 +68,50 @@ namespace Forms_Client.View
             return arr;
         }
 
+        /// <summary>
+        /// Populates the Combobox that shows the departure cities
+        /// </summary>
+        /// <param name="items">An array of city names</param>
         public void PopulateFromBox(string[] items)
         {
             ToBox.Items.Clear();
             FromBox.Items.AddRange(items);
         }
+
+        /// <summary>
+        /// populates the Combobox that shows the Arrival cities
+        /// </summary>
+        /// <param name="items">An array of city names</param>
         public void PopulateToBox(string[] items)
         {
             ToBox.Items.Clear(); // makes sure the list is cleared before adding items, this is to avoid duplicates
             ToBox.Items.AddRange(items);
         }
 
+        /// <summary>
+        /// Gets the text in the FromBox Combobox and makes it publicly visible
+        /// </summary>
         public String FromBoxText
         {
             get { return FromBox.Text; }
         }
 
+        /// <summary>
+        /// Gets the text in the ToBox Combobox and makes it publicly visible
+        /// </summary>
         public String ToBoxText
         {
             get { return ToBox.Text; }
         }
 
+        /// <summary>
+        /// Updates the ToBox Combobox whenever the FromBox Combobox's selection changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FromBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            _presenter.ToBoxText();
+            _presenter.PopulateToList();
         }
     }
 }
