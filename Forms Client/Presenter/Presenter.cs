91e6d18e95d69f7c83e4a20a4028a2d0bc4d7d66
Nikolaj Lepka <slench102+git@gmail.com>
2013-05-23 13:42:44 +0200

    Revert "Added Documentation for the GU"
    This reverts commit 9e7565d885a8d7bf9e097eb44790429d8aa1bd0a.

--- Forms Client/Presenter/Presenter.cs
+++ Forms Client/Presenter/Presenter.cs
@@ -20,20 +20,13 @@ namespace Forms_Client.Presenter
         private string[] _filteredArray = null;
 
 
-        /// <summary>
-        /// Creates a new instance of the Presenter and the Proxy
-        /// Then sends a request to the Proxy
-        /// </summary>
+
         private Presenter()
         {
             _prox = new Proxy();
             _resultString = _prox.Request("list cities");
         }
 
-        /// <summary>
-        /// Instance Accessor of the singleton class
-        /// </summary>
-        /// <returns></returns>
         public static Presenter GetInstance()
         {
             if (_instance == null)
@@ -44,54 +37,38 @@ namespace Forms_Client.Presenter
             return _instance;
         }
 
-        /// <summary>
-        /// Saves an instance of the Overview Form
-        /// </summary>
-        public View.Overview_Form OverviewForm { get; set; }
-        /// <summary>
-        /// Saves an instance of the Main Form
-        /// </summary>
-        public View.MainWindow MainForm { get; set; }
+        public View.Overview_Form Overview { get; set; }
+        public View.MainWindow Main { get; set; }
 
-        /// <summary>
-        /// Calls the overview if the Main Form's text comboboxes contain anything
-        /// </summary>
         public void CallOverview()
         {
-            var arr = MainForm.ChosenCities();
+            var arr = Main.ChosenCities();
             if (arr[0] == "" || arr[1] == "")
                 MessageBox.Show("Du mangler at udfylde felterne", "FEJL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else
             {
-                var request = String.Format("search flights {0} {1}", MainForm.FromBoxText, MainForm.ToBoxText);
+                var request = String.Format("search flights {0} {1}", Main.FromBoxText, Main.ToBoxText);
                 _flight = _prox.Request(request);
-                _instance.OverviewForm = new View.Overview_Form();
-                OverviewForm.ShowDialog();
+                _instance.Overview = new View.Overview_Form();
+                Overview.Show();
             }
         }
 
-        /// <summary>
-        /// Removes the Cancel button once the OK button has been clicked
-        /// </summary>
         public void ChangeOverview()
         {
             if (_okButtonState == true)
             {
-                OverviewForm.SetCancelButtonVisibility(false);
+                Overview.SetCancelButtonVisibility(false);
                 _okButtonState = false;
             }
             else
             {
-                OverviewForm.Close();
-                OverviewForm.SetCancelButtonVisibility(true);
+                Overview.Close();
+                Overview.SetCancelButtonVisibility(true);
                 _okButtonState = true;
             }
         }
 
-
-        /// <summary>
-        /// Populates all the labels in the Overview Form
-        /// </summary>
         public void PopulateOverview()
         {
             var response = TCP_Shared.Response<Tuple<String[], Decimal>>.FromSerialized(_flight);
@@ -101,30 +78,24 @@ namespace Forms_Client.Presenter
             var price = response.Value.Item2;
             var description = String.Format("Din rejse:\n\n{0}", String.Join("\n\n", item1));
 
-            OverviewForm.DescriptopnLabelText(description);
-            OverviewForm.PriceLabelText(price);
+            Overview.DescriptopnLabelText(description);
+            Overview.PriceLabelText(price);
         }
 
-        /// <summary>
-        /// Fills the FromBox Combobox with the list of cities from the server
-        /// </summary>
-        public void PopulateFromList()
+        public void PopulateLists()
         {
             if (_resultString != null)
             {
                 var responseString = TCP_Shared.Response<String[]>.FromSerialized(_resultString);
                 _response = responseString.Value;
-                MainForm.PopulateFromBox(_response);
+                Main.PopulateFromBox(_response);
             }
         }
 
-        /// <summary>
-        /// Fills the ToBox Combobox with the list of cities from the server
-        /// </summary>
-        public void PopulateToList()
+        public void ToBoxText()
         {
-            _filteredArray = _response.Where(x => x != MainForm.FromBoxText).ToArray();
-            MainForm.PopulateToBox(_filteredArray);
+            _filteredArray = _response.Where(x => x != Main.FromBoxText).ToArray();
+            Main.PopulateToBox(_filteredArray);
         }
     }
 }
--- Forms Client/View/Overview Form.cs
+++ Forms Client/View/Overview Form.cs
@@ -14,78 +14,41 @@ namespace Forms_Client.View
     {
         private Presenter.Presenter _presenter = null;
 
-        /// <summary>
-        /// Initializes a new instance of the Overview Form
-        /// and gets the instance of the Presenter.Presenter class
-        /// </summary>
         public Overview_Form()
         {
             InitializeComponent();
             _presenter = Presenter.Presenter.GetInstance();
         }
 
-        /// <summary>
-        /// Populates the form with all the data pulled from the Presenter
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Overview_Form_Shown(object sender, EventArgs e)
         {
             _presenter.PopulateOverview();
         }
 
-        /// <summary>
-        /// Closes the form
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void CancelButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        /// <summary>
-        /// Triggers the ChangeOverview method in the Presenter
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void OKButton_Click(object sender, EventArgs e)
         {
             _presenter.ChangeOverview();
         }
 
-        /// <summary>
-        /// Changes the visibility of the Cancel button
-        /// </summary>
-        /// <param name="visibility"></param>
         public void SetCancelButtonVisibility(bool visibility)
         {
             CancelButton.Visible = visibility;
         }
 
-        /// <summary>
-        /// Saves a new instance of the form when the form is closing
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Overview_Form_FormClosing(object sender, FormClosingEventArgs e)
         {
-            _presenter.OverviewForm = new Overview_Form();
+            _presenter.Overview = new Overview_Form();
         }
 
-        /// <summary>
-        /// Sets the text that will be displayed in the DescriptionLabel
-        /// </summary>
-        /// <param name="text"></param>
         public void DescriptopnLabelText(string text)
         {
             DescriptionLabel.Text =  text;
         }
-
-        /// <summary>
-        /// Sets the text that will be displayed in the PriceLabel
-        /// </summary>
-        /// <param name="price"></param>
         public void PriceLabelText(decimal price)
         {
             PriceLabel.Text = "Pris:\n\n" + price.ToString() +" kr.";
